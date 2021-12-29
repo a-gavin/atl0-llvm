cd6fd4477204738a39c057e16bc7a34b81fc8dd1
Alex Gavin <a_gavin@icloud.com>
2021-12-29 23:36:19 +0000

    Setup for ast dev

---
+++ src/ast/include/ast.hh
@@ -0,0 +1,54 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace ast {
+    // ExprAST - Base class for all expression nodes.
+    class ExprAST {
+    public:
+        virtual ~ExprAST() = default;
+    };
+
+    // NumberExprAST - Expression class for numeric literals like "1.0".
+    class NumberExprAST : public ExprAST {
+        double Val;
+
+    public:
+        NumberExprAST(double Val) : Val(Val) {}
+    };
+
+    // VariableExprAST - Expression class for referencing a variable, like "a".
+    class VariableExprAST : public ExprAST {
+        std::string Name;
+
+    public:
+        VariableExprAST(const std::string &Name) : Name(Name) {}
+    };
+
+    // BinaryExprAST - Expression class for a binary operator.
+    class BinaryExprAST : public ExprAST {
+        char Op;
+        std::unique_ptr<ExprAST> LHS, RHS;
+
+    public:
+        BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                        std::unique_ptr<ExprAST> RHS)
+            : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    };
+}
--- src/include/defs.h
+++ src/include/defs.h
@@ -10,26 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Verifier.h"
-#include <algorithm>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
+#include "ast.hh"
 
 /* Global function prototypes. */
 /* From scan.cc */
